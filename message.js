8e64ce417662d4f4a510faaa1feab9d7d6f9d7f8
Adisdzaky <adisdzakyrivai@gmail.com>
2024-04-02 17:28:50 +0700

    Add files via upload

---
+++ message.js
@@ -0,0 +1,65 @@
+require("./settings")
+msg = {
+    paramkey: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Apikey Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+  paramnumber: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "number Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+  paramprompt: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "prompt Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+  paramseed: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "seed Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+    paramquery: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Query Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+    paramtext: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Text Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+    paramtext2: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Text2 Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+    paramurl: {
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Url Parameter Tidak Ditemukan",
+    	maintanied_by: "RelixOfficial"
+    },
+    nodata: {
+    	status: "Error",
+    	code: 404,
+    	message: "Error Data Tidak Di Temukan",
+    	maintanied_by: "RelixOfficial"
+    },
+    error: {
+    	status: false,
+    	code: 404,
+    	message: '404 ERROR',
+    	maintanied_by: "RelixOfficial"
+    }
+}
+
+              
